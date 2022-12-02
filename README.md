38d7f902133c42dba2249fe635105178a362bcc7
brandlab-au <jon@brandlab.co.uk>
2022-12-02 11:30:46 +0000

    Start work on app

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+Jonathon starts work on 2/12/2002
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Jon and James Rock</code> and save to reload.
+          Bro <code>Jon and James Rock</code> lets see .
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Jon
         </a>
       </header>
     </div>
